468fe1df2384bae0494e97600c631fb74ae1cb8c
mnmndevlol <mnmndevlol@gmail.com>
2021-07-08 15:08:37 +0900

    asdqwd 빠꾸빠꾸

--- _pragmatic/settings/deploy.py
+++ _pragmatic/settings/deploy.py
@@ -1,11 +1,11 @@
 from .base import *
 
-def read_secret(secret_name):
-    file = open('/run/secrets/' + secret_name)
-    secret = file.read()
-    secret = secret.rstrip().lstrip()
-    file.close()
-    return secret
+# def read_secret(secret_name):
+#     file = open('/run/secrets/' + secret_name)
+#     secret = file.read()
+#     secret = secret.rstrip().lstrip()
+#     file.close()
+#     return secret
 
 env = environ.Env(
     # set casting, default value
@@ -19,7 +19,8 @@ environ.Env.read_env(str(BASE_DIR.joinpath('.env')))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+# SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -33,7 +34,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': read_secret('MYSQL_PASSWORD'),
+        'PASSWORD': 'roqkf1234',
         'HOST': 'mariadb',
         'PORT': '3306',
     }
